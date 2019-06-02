131d1914b291dd208c4b51b64e32c2dfc6c4c652
k.borodulin.stc <k.borodulin.stc@innopolis.university>
2019-06-02 20:22:22 +0500

    task16. Реверс массива

--- src/main/java/ru/kborodulin/task16/App.java
+++ src/main/java/ru/kborodulin/task16/App.java
@@ -7,4 +7,31 @@ package ru.kborodulin.task16;
  * Вывести массив в консоль до и после вызова метода по реверсу массива
  */
 public class App {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        // Массив до реверса
+        printArray(array);
+
+        // Реверс
+        reverseArray(array);
+
+        // Массив после реверса
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (int index = 0; index < array.length; index++) {
+            System.out.print(array[index] + " ");
+        }
+        System.out.println("");
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tmp;
+        }
+    }
 }
